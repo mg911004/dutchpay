5ff02394a181550f2e1a684fdadfe31c009610de
leeyegu <mg911003@naver.com>
2022-09-17 20:02:32 +0900

    quasar 추가

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,44 +1,14 @@
 <template>
-    <div class="hello">
-        <h1>{{ msg }}</h1>
-        <input type="text" placeholder="인원 수 입력" v-model="all"/>
-		<input class="input is-success" type="text" placeholder="Success input"/>
-    </div>
+  <q-page class="flex flex-center">
+    <img alt="Quasar logo" src="../assets/logo.svg" style="width: 200px; height: 200px">
+  </q-page>
 </template>
+
+<style>
+</style>
 
 <script>
 export default {
-	name: 'HelloWorld',
-	props: {
-		msg: String
-	},
-	data(){
-		return {
-			all : 0
-		}
-	},
-	methods :{
-		push(){
-			alert(this.all)
-		}
-	}
+  name: 'HelloWorld'
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
